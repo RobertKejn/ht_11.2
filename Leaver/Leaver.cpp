901ac424724c20456c63fec7dc19091258e3ad79
Robert Kein <robertkejn@gmail.com>
2022-12-14 23:34:02 +0400

    initial commit

---
+++ Leaver/Leaver.cpp
@@ -0,0 +1,4 @@
+#include "Leaver.h"
+std::string Leaver::LeaveString(std::string s) {
+	return "До свидания, " + s + "!";
+}
---
+++ LeaverClient/LeaverClient.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include <string>
+#include "Leaver.h"
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << "Ваше имя: ";
+    std::string s;
+    std::getline(std::cin, s);
+    Leaver leaver;
+    std::cout << leaver.LeaveString(s);
+}
